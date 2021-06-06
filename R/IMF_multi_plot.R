eaf8722d0dced7a969794c97a33643f1684257d7
unknown <mejriseifeddine21@gmail.com>
2021-06-06 08:48:56 +0100

    first commit

--- R/IMF_multi_plot.R
+++ R/IMF_multi_plot.R
@@ -16,11 +16,7 @@
 IMF_multi_plot<-function(ts){
   nb_imfs<-VVD::IMF_number(ts)
   ts_imf_df<-VVD::IMF_maker(ts,nb_imfs)
-  for (i in 1:nb_imfs) {
-    plot(seq(1,length(ts_imf_df[,i])),ts_imf_df[,i] , type = "l",
-         main = paste("IMF",i) , xlab = "Time", ylab = "Value", col="blue3")
-
-  }
+  stats::plot.ts(ts_imf_df)
 }
 
 
